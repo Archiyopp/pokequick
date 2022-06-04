fbf1b9e3d2c7ad9e1fa2496f3fa53b1483675a7f
Archiyopp <crafernandezbe@unal.edu.co>
2022-06-04 18:13:34 -0500

    style: agregar nuevos colores y clase css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bwhite: "#fafafa",
+        "poke-blue": "#2a75bb",
+        "poke-red": "#ff0000",
+        "poke-yellow": "#ffcb05",
+      },
+    },
   },
   plugins: [],
 };
